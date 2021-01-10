e2b5ba4e1eba5e35cf15794094652192531d584c
Алексей Котов <zabiyaka39@gmail.com>
2021-01-10 20:08:54 +0300

    Создание

---
+++ Statistic/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Statistic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
